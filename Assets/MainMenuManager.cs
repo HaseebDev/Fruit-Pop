d6a53b1b5dc051bf525e94c19c7e519528f7a955
hussnain-lodhi <razamurtazano1@gmail.com>
2024-05-22 21:06:16 +0500

    custom daily rewards , Shop logics done

---
+++ Assets/MainMenuManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public GameObject mainMenuPanel;
+    public GameObject shopPanel;
+    public GameObject storePanel;
+    public GameObject rewardClaimPanel;
+    public GameObject dailyRewardPanel;
+    public GameObject spinPanel;
+}
